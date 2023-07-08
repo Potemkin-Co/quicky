888764c6344bdabd59ec36d18f44e26329372394
Alexander Potemkin <2323840+alexander-potemkin@users.noreply.github.com>
2023-07-08 15:15:52 +0200

    Delete nodejs16.sh
    ```
    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
    sudo apt-get install -y nodejs
    ```

--- nodejs16.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Script to install Node.JS v16
-
-set -e
-
-curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install -y nodejs
-node -v
